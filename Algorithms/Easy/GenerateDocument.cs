1291a06b08a11d7db67d6059c5d329b3bb0b88e3
Fred <fmaxgalaxy@gmail.com>
2022-06-15 15:47:09 +0200

    Modified GenerateDocument

--- Algorithms/Easy/GenerateDocument.cs
+++ Algorithms/Easy/GenerateDocument.cs
@@ -2,24 +2,23 @@
 public class GenerateDocument
 {
     /*
-    	Problem Solving:
-		Fully understand the problem
-			Input: string, frequency of char in input must be >= whats required for the document
-			Output: just true or false, not the document itself
+    Fully understand the problem
+	    Input: string, frequency of char in input must be >= whats required for the document
+	    Output: just true or false, not the document itself
 
-		Look for potential simplifications of the problem
-			Check whether enough of each characters are available in the input string. If characters < document, you know it's false.
+    Look for potential simplifications of the problem
+	    Check whether enough of each characters are available in the input string. If characters < document, you know it's false.
 
-		Lay out an abstract path from input to output. Think about what types of solutions might work.
-            Iterate through the characters string and populate a dict with the count of each char
-			Iterate through the document string and check the dict, then decrement the value
+    Lay out an abstract path from input to output. Think about what types of solutions might work.
+        Iterate through the characters string and populate a dict with the count of each char
+	    Iterate through the document string and check the dict, then decrement the value
 
-		Then implement the simplified conceptual solution step by step
-			Simply building the dict for the input and then checking each char in the document string against that was easiest.
+    Then implement the simplified conceptual solution step by step
+	    Simply building the dict for the input and then checking each char in the document string against that was easiest.
 
-		Optimization. Big O analysis. Amortization?
-			Time = n+m
-            Space = c
+    Optimization. Big O analysis. Amortization?
+	    Time = n+m
+        Space = c
     */
 
     public bool GenerateDoc(string characters, string document)
