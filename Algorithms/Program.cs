1685571226fd6ba95fbe311fbbd69d2b26bc433d
Fred <fmaxgalaxy@gmail.com>
2022-06-16 01:56:36 +0200

    Comment cleanup

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -7,8 +7,3 @@ internal class Program
         Console.ReadLine();
     }
 }
-
-// create helper extension methods to display outputs
-// make this proj something epic that you can use for a long while
-// create tests and samples in the file of the algo
-// review the ones you simply import, you at least have understand conceptually whats going on
