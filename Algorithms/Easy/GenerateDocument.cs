19a5714afeebee0e8e73bf2efc8f8decc22705f0
Fred <fmaxgalaxy@gmail.com>
2022-06-15 15:49:57 +0200

    Indentation fixes

--- Algorithms/Easy/GenerateDocument.cs
+++ Algorithms/Easy/GenerateDocument.cs
@@ -3,18 +3,18 @@ public class GenerateDocument
 {
     /*
     Fully understand the problem
-	    Input: string, frequency of char in input must be >= whats required for the document
-	    Output: just true or false, not the document itself
+        Input: string, frequency of char in input must be >= whats required for the document
+        Output: just true or false, not the document itself
 
     Look for potential simplifications of the problem
-	    Check whether enough of each characters are available in the input string. If characters < document, you know it's false.
+        Check whether enough of each characters are available in the input string. If characters < document, you know it's false.
 
     Lay out an abstract path from input to output. Think about what types of solutions might work.
         Iterate through the characters string and populate a dict with the count of each char
         Iterate through the document string and check the dict, then decrement the value
 
     Then implement the simplified conceptual solution step by step
-	    Simply building the dict for the input and then checking each char in the document string against that was easiest.
+        Simply building the dict for the input and then checking each char in the document string against that was easiest.
 
     Optimization. Big O analysis. Amortization?
         Time = n+m
